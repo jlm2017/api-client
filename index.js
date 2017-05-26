c02893d863dd1f9a86e4265c5047e5d8391c0eeb
Arthur Cheysson <arthur@cheysson.fr>
2017-05-26 14:32:01 +0200

    Adapt the client package for the new API

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const createClient = require('./lib/api');
-const filters = require('./lib/filters');
 const exceptions = require('./lib/exceptions');
 
-module.exports = {createClient, filters, exceptions};
+module.exports = {createClient, exceptions};
